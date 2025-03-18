73346376e77e7c06149e4ab9af843080115dabae
Jaden Howard <jaseanhow@gmail.com>
2025-03-18 13:19:38 -0400

    Cameras fully functional. Integrate with ImGUI/ImPlot dashboard is next along with Arduino configuration for Dumping.

--- src/robot_launch/scripts/start_teleop.sh
+++ src/robot_launch/scripts/start_teleop.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-
 set -xe
-
 echo "STARTING ROBOT TELEOPERATION CONTROL!"
-
 ros2 launch robot_launch teleop.launch.py &
 
 echo "STARTING ROBOT CAMERA LIVESTREAM FOOTAGE!"
 
-ffmpeg -f v4l2 -framerate 60 -video_size 1920x1080 -i /dev/video2 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | ffplay -fflags nobuffer -framedrop - &
-ffmpeg -f v4l2 -framerate 60 -video_size 1920x1080 -i /dev/video0 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska udp://localhost:1235 &
-ffmpeg -f v4l2 -framerate 60 -video_size 1920x1080 -i /dev/video1 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska udp://localhost:1236 &
-ffmpeg -f v4l2 -framerate 60 -video_size 1920x1080 -i /dev/video3 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska udp://localhost:1237 &
+# 1080p with minimal latency
+ffmpeg -f v4l2 -input_format mjpeg -framerate 30 -video_size 1920x1080 -i /dev/video0 \
+  -c:v rawvideo -pix_fmt yuv420p \
+  -fflags nobuffer -flags low_delay \
+  -f nut pipe:1 | \
+ffplay -fflags nobuffer -framedrop -flags low_delay \
+  -probesize 32 -analyzeduration 0 -sync ext \
+  -x 640 -y 360 -window_title "Robot Digging Subsystem Camera" \
+  -i - &
+
+  
 
 wait
